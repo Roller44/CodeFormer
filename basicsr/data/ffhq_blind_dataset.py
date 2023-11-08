8bf9fe737246236eb833b761fff20654a04fc62e
Shumin Yao <roller44@163.com>
2023-11-08 06:54:51 +0000

    Encode_param float to int

--- basicsr/data/ffhq_blind_dataset.py
+++ basicsr/data/ffhq_blind_dataset.py
@@ -231,7 +231,9 @@ class FFHQBlindDataset(data.Dataset):
 
             # jpeg
             if self.jpeg_range is not None:
-                jpeg_p = np.random.uniform(self.jpeg_range[0], self.jpeg_range[1])
+                # jpeg_p = np.random.uniform(self.jpeg_range[0], self.jpeg_range[1])
+                # Ref: https://github.com/sczhou/CodeFormer/issues/266#issuecomment-1630336860
+                jpeg_p = np.random.randint(self.jpeg_range[0], self.jpeg_range[1])
                 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_p]
                 _, encimg = cv2.imencode('.jpg', img_in * 255., encode_param)
                 img_in = np.float32(cv2.imdecode(encimg, 1)) / 255.
